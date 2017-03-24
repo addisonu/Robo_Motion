0c2c55cf1620b0d6ac8890bbba492bad83c06739
addisonu <my_wireless_world@yahoo.com>
2017-03-24 02:27:01 -0400

    Change file spacing

--- source/message_sys/test/client_main.cpp
+++ source/message_sys/test/client_main.cpp
@@ -17,12 +17,12 @@
 int main(int argc, char **argv)
 {
 	try{
-	std::string msg;
-	MessageClient client;
-	client.openConnection(msg);
-	client.bindSocket(msg);
-	client.doConnection();
-	client.closeConnection(msg);
+	    std::string msg;
+	    MessageClient client;
+	    client.openConnection(msg);
+	    client.bindSocket(msg);
+	    client.doConnection();
+	    client.closeConnection(msg);
 	}
 	catch(std::exception &e){
 		std::cout << "There was an unkwon error, program terminating" << std::endl;
