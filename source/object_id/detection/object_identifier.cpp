2369e9e1026009f5225e2e260bca0ec5c95cea38
addisonu <my_wireless_world@yahoo.com>
2017-03-24 03:33:38 -0400

    Change file spacing

--- source/object_id/detection/object_identifier.cpp
+++ source/object_id/detection/object_identifier.cpp
@@ -13,20 +13,20 @@
 int main(int argc, char **argv)
 {
 	try{
-	// Load image
-	if(argv[1] == nullptr || argv[1] == '\0'){
-		std::cerr << "No image passed as program argument." << std::endl; 
+		// Load image
+		if(argv[1] == nullptr || argv[1] == '\0'){
+			std::cerr << "No image passed as program argument." << std::endl; 
 		}
 		cv::Mat img;
 		img = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
 
-	// Load classifiers
-	cv::CascadeClassifier vehicle_cascade;
-	vehicle_cascade.load("../classification/vehicle_classifier.xml");
+		// Load classifiers
+		cv::CascadeClassifier vehicle_cascade;
+		vehicle_cascade.load("../classification/vehicle_classifier.xml");
 
-	// Dectect objects
-	std::vector<cv::Rect> all_vehicle;
-	vehicle_cascade.detectMultiScale(img, all_vehicle, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, cv::Size(30, 30));
+		// Dectect objects
+		std::vector<cv::Rect> all_vehicle;
+		vehicle_cascade.detectMultiScale(img, all_vehicle, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, cv::Size(30, 30));
 		
 	}
 	catch(std::exception &e){
