315e14a2152027680305050de6fc294aa90d323b
addisonu <my_wireless_world@yahoo.com>
2017-03-24 02:05:06 -0500

    Start object identification program

---
+++ source/object_id/detection/object_identifier.cpp
@@ -0,0 +1,12 @@
+// AUTHOR: NA
+// DATE: 03/24/17
+// DESC: File to load classifiers and identify objects
+// FILE: object_identifier.cpp
+
+#include "opencv2/objdetect/objdetect.hpp"
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
