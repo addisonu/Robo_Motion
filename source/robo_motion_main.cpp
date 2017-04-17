610c3b4a7affc0c9b635903e04e2b9c246cffe18
addisonu <my_wireless_world@yahoo.com>
2017-04-17 15:28:28 -0500

    Add program entry point

---
+++ source/robo_motion_main.cpp
@@ -0,0 +1,64 @@
+// AUTHOR: NA
+// DATE: 04/17/17
+// DESC: Entry point for program
+// FILE: robo_motion_main.cpp
+
+#include <iostream>
+#include <cstdio>
+#include <exception>
+#include <fstream>
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+int main(int argc, char **argv)
+{
+    // Extract arguments
+    if(argv[1] == nullptr || argv[1] == '\0'){
+            std::cerr << "No image passed as program argument." << std::endl;
+    }
+    try{
+            // Load image
+            cv::Mat img;
+            img = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
+
+            /*
+            // Load classifiers
+            std::vector<std::pair<std::string, cv::CascadeClassifier> > all_classifier;
+            //cv::CascadeClassifier vehicle_cascade;
+            //vehicle_cascade.load("../classification/vehicle_classifier.xml");
+            try{
+                std::ifstream ifs;
+                ifs.open("all_classifier.txt", std::ifstream::in);
+
+                if(ifs.is_open()){
+                    char cclassifier[500];
+                    while(ifs.good()){
+                        ifs.getline(cclassifier, 500);
+                        std::string classifier(cclassifier);
+                        std::size_t pos = classifier.find('|');
+                        cv::CascadeClassifier tmp_cascade;
+                        tmp_cascade.load(classifier.substr(pos + 1));
+                        all_classifier.push_back(std::make_pair(classifier.substr(0, pos + 1), tmp_cascade));
+                    }
+                    ifs.close();
+                }
+            }
+            catch(std::exception &e){
+                std::cerr << "Error: classifiers not read from file all_classifier.txt" << std::endl << e.what() << std::endl;
+            }
+            // Dectect objects
+            for(auto classifier_ele : all_classifier){
+                std::vector<cv::Rect> all_object;
+                classifier_ele.second.detectMultiScale(img, all_object, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, cv::Size(30, 30));
+                if(all_object.size() > 0){
+                    return classifier_ele.first;
+                }
+            }
+                */
+    }
+    catch(std::exception &e){
+            std::cerr << "An unkwown error occurred. Program will now terminate." << std::endl;
+    }
+return 0;
+}
